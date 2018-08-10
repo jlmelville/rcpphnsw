508451bb4d2e656aef08b66843a369dca49a91df
James Melville <jlmelville@gmail.com>
2018-08-10 08:40:34 -0700

    Update to nmslib/hnsw@74ee08f

--- inst/include/hnswalg.h
+++ inst/include/hnswalg.h
@@ -523,9 +523,8 @@ public:
     std::ifstream input(location, std::ios::binary);
 
     // get file size:
-    std::streampos end_p=input.tellg();
     input.seekg(0,input.end);
-    auto total_filesize=input.tellg();
+    std::streampos total_filesize=input.tellg();
     input.seekg(0,input.beg);
 
     readBinaryPOD(input, offsetLevel0_);
