dea410b68fc5f593fa0397b330d1cb0e729c713d
James Melville <jlmelville@gmail.com>
2018-12-14 21:48:35 -0800

    #2: test for 1NN is a matrix

--- tests/testthat/test_get_knn.R
+++ tests/testthat/test_get_knn.R
@@ -14,4 +14,8 @@ res <- get_knn(ui10, k = 4, include_self = TRUE)
 expect_equal(res$idx, self_nn_index4, check.attributes = FALSE)
 expect_equal(res$dist, self_nn_dist4, check.attributes = FALSE, tol = 1e-6)
 
-
+res <- get_knn(ui10, k = 1, include_self = FALSE)
+expect_is(res$idx, "matrix")
+expect_is(res$dist, "matrix")
+expect_equal(res$idx, self_nn_index4[, 2], check.attributes = FALSE)
+expect_equal(res$dist, self_nn_dist4[, 2], check.attributes = FALSE, tol = 1e-6)
