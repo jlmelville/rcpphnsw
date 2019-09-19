afc5f7de9719a48e3a479e4dceef2b0404f4d0d9
James Melville <jlmelville@gmail.com>
2019-09-18 22:14:01 -0700

    Add test for size method.

--- tests/testthat/test_search_one.R
+++ tests/testthat/test_search_one.R
@@ -4,8 +4,14 @@ context("search with small test set")
 # #5: when searching with a pre-built index, k <= "training" set size,
 # not the test set size.
 index <- hnsw_build(ui10)
+# Added a size method so the user can check the index size if needed
+expect_equal(index$size(), 10)
 
 res <- hnsw_search(ui10[1, , drop = FALSE], index, k = 4)
 
 expect_equal(res$idx, self_nn_index4[1, , drop = FALSE], tol = 1e-6)
 expect_equal(res$dist, self_nn_dist4[1, , drop = FALSE], tol = 1e-6)
+
+# An error will be thrown if not enough neighbors can be retrieved
+# in this case due to k > index$size()
+expect_error(hnsw_search(ui10[1, , drop = FALSE], index, k = 500))
