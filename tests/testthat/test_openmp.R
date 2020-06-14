87fa49eb816f0c3546e844e08206a53c5d3833e7
Dmitriy Selivanov <selivanov.dmitriy@gmail.com>
2020-06-14 09:17:41 +0400

    use 3 thread for tests as per CRAN requirements

--- tests/testthat/test_openmp.R
+++ tests/testthat/test_openmp.R
@@ -15,22 +15,22 @@ ind_1 <- hnsw_build(
   n_threads = 1
 )
 
-ind_4 <- hnsw_build(
+ind_2 <- hnsw_build(
   x,
   distance = "euclidean",
   M = 16,
   ef = 200,
   verbose = FALSE,
   progress = "bar",
-  n_threads = 4
+  n_threads = 2
 )
 
 knn_1 = hnsw_search(x, ind_1, k = 5)
-knn_4 = hnsw_search(x, ind_4, k = 5)
+ind_2 = hnsw_search(x, ind_4, k = 5)
 # Seems index which was built using more than 1 thread is not determenistic
 # but in general the difference between index built with 1 thread and
 # many threads should be small
-expect_lt(mean(knn_1$dist - knn_4$dist), 1e-4)
+expect_lt(mean(knn_1$dist - ind_2$dist), 1e-4)
 
 # same check for indices
-expect_lt(mean(knn_1$idx != knn_4$idx), 1e-2)
+expect_lt(mean(knn_1$idx != ind_2$idx), 1e-2)
