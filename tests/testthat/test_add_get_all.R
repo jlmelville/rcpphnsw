b27d82986d1e9b12a7c8f1a4756eff5a5652cd5b
James Melville <jlmelville@gmail.com>
2022-07-24 14:53:55 -0700

    add some test coverage

--- tests/testthat/test_add_get_all.R
+++ tests/testthat/test_add_get_all.R
@@ -2,6 +2,10 @@ library(RcppHNSW)
 context("add/get matrix")
 
 ann <- new(HnswL2, ncol(ui10), nrow(ui10), M = 200, ef = 16)
+
+# error thrown if empty index is searched
+expect_error(ann$getAllNNs(ui10, k = 4), "(?i)unable to find")
+
 ann$addItems(ui10)
 res <- ann$getAllNNsList(ui10, k = 4, include_distances = TRUE)
 expect_equal(res$item, self_nn_index4, check.attributes = FALSE)
--- tests/testthat/test_search_one.R
+++ tests/testthat/test_search_one.R
@@ -16,6 +16,13 @@ expect_equal(res$dist, self_nn_dist4[1, , drop = FALSE], tol = 1e-6)
 # in this case due to k > index$size()
 expect_error(hnsw_search(ui10[1, , drop = FALSE], index, k = 500))
 
+# Neighbors of one item
+expect_equal(index$getNNsList(ui10[1, ], 4, FALSE)$item, self_nn_index4[1, ])
+nbrs_with_distances <- index$getNNsList(ui10[1, ], 4, TRUE)
+expect_equal(nbrs_with_distances$item, self_nn_index4[1, ])
+expect_equal(nbrs_with_distances$distance, self_nn_dist4[1, ]^2, tol = 1e-6)
+expect_error(index$getNNsList(ui10[1, ], 15, FALSE), "(?i)unable to find")
+
 # Test deletion
 index$markDeleted(1)
 res <- hnsw_search(ui10[1, , drop = FALSE], index, k = 4)
