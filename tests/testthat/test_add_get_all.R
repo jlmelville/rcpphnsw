bf30947f6c6000f677e811e16b5cfcd575e9e952
James Melville <jlmelville@gmail.com>
2022-07-31 16:58:41 -0700

    reformatting

--- tests/testthat/test_add_get_all.R
+++ tests/testthat/test_add_get_all.R
@@ -9,8 +9,10 @@ expect_error(ann$getAllNNs(ui10, k = 4), "(?i)unable to find")
 ann$addItems(ui10)
 res <- ann$getAllNNsList(ui10, k = 4, include_distances = TRUE)
 expect_equal(res$item, self_nn_index4, check.attributes = FALSE)
-expect_equal(sqrt(res$dist), self_nn_dist4, check.attributes = FALSE,
-             tol = 1e-6)
+expect_equal(sqrt(res$dist), self_nn_dist4,
+  check.attributes = FALSE,
+  tol = 1e-6
+)
 
 res <- ann$getAllNNsList(ui10, k = 4, include_distances = FALSE)
 expect_equal(res$item, self_nn_index4, check.attributes = FALSE)
@@ -24,8 +26,10 @@ ui10t <- t(ui10)
 ann2$addItemsCol(ui10t)
 res <- ann2$getAllNNsListCol(ui10t, k = 4, include_distances = TRUE)
 expect_equal(t(res$item), self_nn_index4, check.attributes = FALSE)
-expect_equal(sqrt(t(res$dist)), self_nn_dist4, check.attributes = FALSE,
-             tol = 1e-6)
+expect_equal(sqrt(t(res$dist)), self_nn_dist4,
+  check.attributes = FALSE,
+  tol = 1e-6
+)
 
 res <- ann2$getAllNNsListCol(ui10t, k = 4, include_distances = FALSE)
 expect_equal(t(res$item), self_nn_index4, check.attributes = FALSE)
--- tests/testthat/test_build_search.R
+++ tests/testthat/test_build_search.R
@@ -12,5 +12,7 @@ ui10_col <- t(ui10)
 index2 <- hnsw_build(ui10_col, byrow = FALSE)
 res_col <- hnsw_search(ui10_col, index2, k = 4, byrow = FALSE)
 expect_equal(t(res_col$idx), self_nn_index4, check.attributes = FALSE)
-expect_equal(t(res_col$dist), self_nn_dist4, check.attributes = FALSE,
-             tol = 1e-6)
+expect_equal(t(res_col$dist), self_nn_dist4,
+  check.attributes = FALSE,
+  tol = 1e-6
+)
